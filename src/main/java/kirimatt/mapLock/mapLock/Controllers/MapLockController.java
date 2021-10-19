c4aac2468239e8cb6604ca318c521e48054b8f52
kirimatt <wakarimasen_deshita@mail.ru>
2021-10-19 18:00:16 +0600

    Initial commit

---
+++ src/main/java/kirimatt/mapLock/mapLock/Controllers/MapLockController.java
@@ -0,0 +1,20 @@
+package kirimatt.mapLock.mapLock.Controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lock")
+public class MapLockController {
+
+    @GetMapping("/")
+    public ResponseEntity getLock() {
+        try {
+            return ResponseEntity.ok("РАБОТАЕМ!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+}
---
+++ src/test/java/kirimatt/mapLock/mapLock/MapLockApplicationTests.java
@@ -0,0 +1,13 @@
+package kirimatt.mapLock.mapLock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapLockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
