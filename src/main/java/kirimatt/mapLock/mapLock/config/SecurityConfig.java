67a63fa35b8cf6508a830215e33a9c10dd89d04b
kirimatt <wakarimasen_deshita@mail.ru>
2022-03-30 17:36:44 +0600

    new release

--- src/main/java/kirimatt/mapLock/mapLock/config/SecurityConfig.java
+++ src/main/java/kirimatt/mapLock/mapLock/config/SecurityConfig.java
@@ -31,6 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure()
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
